716583402d441187b97a2d602fec2c7bc1524cab
paprika <paprika@hognjiaodeiMac.lan>
2018-12-10 08:02:52 +0800

    加入webpack

---
+++ app/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>Hello React!
+        <i className="iconfont icon-wuliao"></i>
+      </div>
+    );
+  }
+}
---
+++ app/main.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './main.css';
+
+// const renderDom = Component => {
+//   ReactDOM.render(
+//     <Component />,
+//     document.getElementById('root')
+//   );
+// }
+
+// renderDom(App);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
